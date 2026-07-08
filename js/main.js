5d51d6a0ff4e2fe02cf4abf90813895188080d77
Preppy Fairy Studio <Killyrye@gmail.com>
2026-07-08 02:02:09 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,63 @@
 console.log("Portal Architect loaded");
 
+
 const button = document.getElementById("generateBtn");
+
 
 button.addEventListener("click", function(){
 
-    const x = document.getElementById("motherX").value;
+    const overworldX = Number(
+        document.getElementById("motherX").value
+    );
 
-    const z = document.getElementById("motherZ").value;
+    const overworldZ = Number(
+        document.getElementById("motherZ").value
+    );
 
-    const radius = document.getElementById("radius").value;
+    const radius = Number(
+        document.getElementById("radius").value
+    );
 
 
-    document.getElementById("output").innerHTML = `
+    const netherX = Math.floor(overworldX / 8);
 
-    <h2>Mother Portal</h2>
+    const netherZ = Math.floor(overworldZ / 8);
 
-    <p>X: ${x}</p>
 
-    <p>Z: ${z}</p>
 
-    <p>Radius: ${radius}</p>
+    document.getElementById("results").innerHTML = `
+
+        <div class="portal-result">
+
+            <h3>🟪 Mother Portal</h3>
+
+            <p>
+            Overworld:
+            <br>
+            X: ${overworldX}
+            <br>
+            Z: ${overworldZ}
+            </p>
+
+
+            <p>
+            Nether:
+            <br>
+            X: ${netherX}
+            <br>
+            Z: ${netherZ}
+            </p>
+
+
+            <p>
+            Grid Radius:
+            ${radius}
+            </p>
+
+
+        </div>
 
     `;
+
 
 });
